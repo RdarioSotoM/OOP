9cebaecd62e56f52ff57de5be07c458248c47ac0
Dario <rubendario.sotomontana@tiernoparla.es>
2023-01-17 22:21:38 +0100

    Primer objeto Circulo creado.

---
+++ src/main/java/daw/ejercicios/Circulo/Circulo.java
@@ -0,0 +1,38 @@
+package daw.ejercicios.Circulo;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
+
+public class Circulo {
+    private double radio = 1.0;
+
+    public Circulo() {
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return this.radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getArea() {
+        return pow(this.radio, 2) * PI;
+    }
+
+    public double getCircunferencia() {
+        return 2 * PI * this.radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Cirulo [radio = " + radio + " ]";
+    }
+
+    
+}
---
+++ src/main/java/daw/ejercicios/Circulo/Lanzador.java
@@ -0,0 +1,11 @@
+package daw.ejercicios.Circulo;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        Circulo circulito = new Circulo();
+        System.out.println(circulito);
+    }
+
+}
+
